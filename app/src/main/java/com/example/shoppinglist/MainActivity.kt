8e4ef2adba9f16dd1664d331ff54fc1801b44589
Maevex <ilhamku2005@gmail.com>
2025-12-29 21:55:36 +0700

    finishing alert dialog, creating interface for lazycolumn

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shoppinglist.ui.theme.ShoppingListTheme
@@ -30,7 +31,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShoppingListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    containerColor = Color.White) { innerPadding ->
                     ShoppingListApp(modifier = Modifier.padding(innerPadding))
 
                 }
--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -5,17 +5,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,10 +38,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shoppinglist.ui.theme.ShoppingListTheme
 
+import androidx.compose.ui.text.TextStyle
 data class ShoppingItem(
     val id:Int,
     var name: String,
@@ -41,6 +57,8 @@ data class ShoppingItem(
 fun ShoppingListApp(modifier: Modifier = Modifier){
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var ShowDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQty by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -52,7 +70,11 @@ fun ShoppingListApp(modifier: Modifier = Modifier){
 //                        Text("Halo")
 //                        Text("Halo")
 //                        Text("Halo")
-        Button(onClick = {ShowDialog = true},
+        Button(onClick = {ShowDialog = true},//trigger muncul alertdialog
+            border = BorderStroke(2.dp, Color.Black),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent // button ngga bisa bawa parameter Color.black untuk colorsnya
+            ),
             modifier = Modifier.align(Alignment.CenterHorizontally
             ))
         {
@@ -65,14 +87,111 @@ fun ShoppingListApp(modifier: Modifier = Modifier){
                 .padding(16.dp)
         ) {
             items(sItems){
-
+                ShoppingListItem(it, {}, {})
             }
         }
 
     }
-    if (ShowDialog){
-        AlertDialog(onDismissRequest = {ShowDialog = false}){
-            Text("ello")
+    if (ShowDialog){//jika show dialog true, tampilkan alert
+
+        AlertDialog(onDismissRequest = {ShowDialog = false},
+            containerColor = Color.White,
+            confirmButton = {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+
+                ) {
+                    Button(onClick = {
+                        if (itemName.isNotBlank()){
+                            val newItem = ShoppingItem(id=sItems.size+1,
+                                name = itemName,
+                                quantity = itemQty.toInt())
+                            sItems = sItems + newItem
+                            ShowDialog = false
+                            itemName = ""
+                        }
+                    },
+                    border = BorderStroke(2.dp, Color.Black),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent // button ngga bisa bawa parameter Color.black untuk colorsnya
+                        )) {
+                        Text("Add")
+                    }
+                    Button(onClick = {ShowDialog = false},
+                        border = BorderStroke(2.dp, Color.Black),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent // button ngga bisa bawa parameter Color.black untuk colorsnya
+                        )
+                    ) {
+                        Text("Cancel")
+                    }
+                }
+            },
+            title = {Text("Add Shopping Item", color = Color.Black)},
+            text = {
+                Column {
+                    OutlinedTextField(value = itemName, onValueChange = {itemName = it},
+                        singleLine = true,
+                        shape = RoundedCornerShape(20),
+                        textStyle = TextStyle(color = Color.Black),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp))
+
+
+                    OutlinedTextField(value = itemQty, onValueChange = {itemQty = it},
+                        singleLine = true,
+                        shape = RoundedCornerShape(20),
+                        textStyle = TextStyle(color = Color.Black),
+                        modifier = Modifier.fillMaxWidth().padding(8.dp))
+                }
+            }
+            )
+
         }
+    }
+
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(width = 2.dp, Color.Black,),
+                shape = RoundedCornerShape(20)
+            )
+            .padding(20.dp), //konten dengan border
+        horizontalArrangement = Arrangement.SpaceBetween
+
+    ) {
+        Text(item.name, modifier = Modifier.padding(8.dp)) // nampilin data name yang habis diinput
+        Text("Qty : ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row (
+            modifier = Modifier.padding(8.dp)
+        ) {
+            //Button edit
+            IconButton(onClick = onEditClick) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit"
+
+                )
+            }
+            //button delete
+            IconButton(onClick = onDeleteClick) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete"
+
+                )
+            }
+        }
+
     }
 }
